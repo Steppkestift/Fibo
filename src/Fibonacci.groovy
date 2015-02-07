9206e7322018a3bfa0dd3683c1e25f7846900ea4
Steppkestift <heiko.linke@posteo.de>
2015-02-07 17:53:48 +0100

    test

--- src/Fibonacci.groovy
+++ src/Fibonacci.groovy
@@ -1,12 +1,8 @@
-/**
- * Created by heiko-linke on 07.02.15.
- */
-current = 1
-next =    1
-10.times {
-    print current + ' '
-    newCurrent = next
-    next = next + current
-    current = newCurrent
+def totalClinks = 0
+def partyPeople = 100
+1.upto(partyPeople) {
+        guestNumber ->
+        clinksWithQuest = guestNumber-1
+        totalClinks += clinksWithQuest
 }
-println ''
+assert totalClinks == {partypeople*(partyPeople-1)}/2
